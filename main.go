f54b50e4dd8be711b2b1fc1c07ca1b331ba7bdb6
xbuyan <kevobuyan@gmail.com>
2026-04-09 09:44:49 +0300

    feat: add SHA-256 file hashing

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -31,4 +34,25 @@ func main() {
 	fmt.Println("Name", info.Name())
 	fmt.Println("Size", info.Size())
 	fmt.Println("Modified:", info.ModTime().Format("2006-01-02 15:04:05"))
+
+	hash, err := hashFile(filepath)
+	if err != nil {
+		fmt.Println("Error hashing file:", err)
+		return
+	}
+	fmt.Println("SHA-256: ", hash)
+}
+
+func hashFile(filepath string) (string, error) {
+	content, err := os.Open(filepath)
+	if err != nil {
+		return "", err
+	}
+	defer content.Close()
+	hasher := sha256.New()
+
+	io.Copy(hasher, content)
+	bytes := hasher.Sum(nil)
+
+	return hex.EncodeToString(bytes), nil
 }
